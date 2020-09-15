bf98c77d1b169942fabd9ced7f1d35e37ffcd01b
maarten.bassier <maarten.bassier@kuleuven.be>
2020-09-15 15:44:52 +0200

    test

---
+++ Saiga/src/HelloDynamo - Copy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.DesignScript.Runtime;
+using Autodesk.DesignScript.Geometry;
+
+namespace Saiga
+{
+    /// <summary>
+    /// Hello World basic sample
+    /// </summary>
+    public static class HelloDynamo2
+    {
+        /// <summary>
+        /// Prints a friendly greeting
+        /// </summary>
+        /// <param name="name">Name of the person to greet</param>
+        /// <returns>A greeting</returns>
+        public static string SayHello2(string name)
+        {
+            return String.Format("Hello {0}!", name);
+        }
+    }
+}
