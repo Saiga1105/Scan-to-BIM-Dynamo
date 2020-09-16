73718bbe4f6d6b948106a470f8ed6be01a540761
maarten.bassier <maarten.bassier@kuleuven.be>
2020-09-16 12:03:06 +0200

    trying to figure out namespace organisation

---
+++ Saiga/src/1.PointClouds/ImportFile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.DesignScript.Runtime;
+using Autodesk.DesignScript.Geometry;
+using rv = Autodesk.Revit.DB;
+using Autodesk.Revit.DB.PointClouds;
+using mw = MATLAB;
+using Rhino;
+using rc = Rhino.Geometry;
+
+namespace Saiga.PointClouds  /// this is our main namespace
+{
+    /// <summary>
+    /// Imports an .rcs point cloud 
+    /// </summary>
+    public class ImportFile   /// this is our nested namespace
+    {
+        /// <summary>
+        /// Imports an .rcs point cloud 
+        /// </summary>
+        /// <param fileName="fileName">Full file path of the point cloud to import </param>
+        /// <returns>pointCloud</returns>
+        public static string ImportE57 (string fileName)
+        {
+            float x = 2.5F, y = 0.0F, z = 0.0F;
+            var coord= new rv.XYZ( x,  y,  z);
+            var test = new rv.PointClouds.CloudPoint(x, y, z,255);
+            rc.PointCloud pointCloud = new rc.PointCloud();
+
+            return String.Format("E57 imported", fileName);
+
+        }
+
+        public static string ImportRCS(string fileName)
+        {
+            float x = 2.5F, y = 0.0F, z = 0.0F;
+            var coord = new rv.XYZ(x, y, z);
+            var test = new rv.PointClouds.CloudPoint(x, y, z, 255);
+            rc.PointCloud pointCloud = new rc.PointCloud();
+
+            return String.Format("RCS imported", fileName);
+
+        }
+
+    }
+}
---
+++ Saiga/src/1.PointClouds/Selection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.DesignScript.Runtime;
+using Autodesk.DesignScript.Geometry;
+using rv=Autodesk.Revit.DB;
+using Autodesk.Revit.DB.PointClouds;
+using mw = MATLAB;
+using Rhino;
+using rc = Rhino.Geometry;
+
+
+namespace Saiga.PointClouds
+{
+    /// <summary>
+    /// Retrieve points from box selection
+    /// </summary>
+    public class Selection 
+    {
+        /// <summary>
+        /// Returns a pointCloud within a boundingBox selection. API is restricted to 1M points per boundingBox
+        /// </summary>
+        /// <param fileName="fileName">Full file path of the point cloud to import </param>
+        /// <returns>pointCloud</returns>
+        public static string BoundingBoxSelection( string fileName)
+        {
+            float x = 2.5F, y = 0.0F, z = 0.0F;
+            var coord= new rv.XYZ( x,  y,  z);
+            var test = new CloudPoint(x, y, z,255);
+            
+
+            return String.Format("I've selected a point cloud!", fileName);
+        }
+       
+    }
+}
